345ae1a38ce4bc22e81ede5169aad279fe32e3ea
dbalampanidis <dbalampanidis@gmail.com>
2022-08-02 00:50:49 +0300

    add basic styling

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)} `
+            title: `${sample(descriptors)} ${sample(places)} `,
+            image: 'https://source.unsplash.com/collection/483251/',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam illum sed voluptatem natus qui. Saepe, explicabo molestias nulla qui quas iure, ea omnis error beatae alias debitis, vel esse? Odio!',
+            price
         })
         await camp.save();
     }
